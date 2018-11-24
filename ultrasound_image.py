48017fb825a2ae5570816ff5a290e58cae1a9921
Mackenzie Wilson <mackenziewilson@Mackenzies-MacBook-Air.local>
2018-11-24 16:32:17 -0500

    Changed python image handling

--- ultrasound_image.py
+++ ultrasound_image.py
@@ -4,13 +4,14 @@
 Created on Wed Nov 14 21:43:58 2018
 
 @author: mackenziewilson
+
 """
 
+# Imports
 import serial
-import matplotlib.pyplot as plt
-import matplotlib.colors as colour
 import numpy as np
 import PIL.Image as im
+import time 
 
 
 # Set up serial to read from Arduino
@@ -18,30 +19,40 @@ port = '/dev/cu.usbmodem1421'
 ser = serial.Serial(port)
 
 # Image size
-cols = 5
-rows = 5
+# MAKE SURE THESE VALUES MATCH UP WITH ARDUINO CODE
+horiz_steps = 6
+vert_steps = 4
+image_array = np.zeros( (vert_steps*2, horiz_steps) )
+
+# Arrays to hold data (will be spatially upside down)
+data_top = np.zeros((vert_steps, horiz_steps))
+data_bottom = np.zeros((vert_steps, horiz_steps))
 
 # Read from serial port until enough data captured
-dataTop = []
-dataBottom = []
-for i in range(rows*cols):
-    valBottom, valTop = (np.uint8( ser.readline().strip() )).split(',')
-    dataBottom.append(valBottom)
-    dataTop.append(valTop)
+for i in range(vert_steps):
+    for j in range(horiz_steps):
+        data_bottom[i,j] = np.uint8( ser.readline().strip() )
+        data_top[i,j] = np.uint8( ser.readline().strip() )
 
 ser.close() # CLOSE THE SERIAL PORT YA DUMMY 
 
-# Convert data to 2D array
-data_array = np.array(data)
-data_array = data_array[::-1].reshape((rows,cols))
-#print(data_array)
+# Combine two transducer data arrays into one image_array
+# Keep in mind the data arrays are upside down from how we want the image
+for i in range(int(vert_steps/2)):
+    image_array[i*4] = data_bottom[i*2]
+    image_array[i*4 + 1] = data_bottom[i*2 + 1]
+    image_array[i*4 + 2] = data_top[i*2]
+    image_array[i*4 + 3] = data_top[i*2 + 1]
 
-# Normalize data to pixel values
+image_array_final = image_array[::-1]
+
+
+# Normalize image array to pixel values
 pixel_min = 0
 pixel_max = 255
-data_min = np.min(data_array)
-new_data_range = max(np.max(data_array) - data_min, 1)
-normalized_data_array = ( (data_array - data_min) * (255/new_data_range) ).astype(np.uint8)
+data_min = np.min(image_array_final)
+new_data_range = max(np.max(image_array_final) - data_min, 1)
+normalized_image_array = ( (image_array_final - data_min) * (255/new_data_range) ).astype(np.uint8)
 
 '''
 # For ease of use
@@ -55,8 +66,13 @@ normalized_data_array = np.array(
 #print("")
 #print(normalized_data_array)
 
-img = im.fromarray(normalized_data_array)
+img = im.fromarray(normalized_image_array)
 img.show()
-img.save('image_1.bmp')
+
+timestamp = time.strftime("%Y%m%d-%H%M%S")
+img.save("saved_images/image-" + timestamp + ".bmp")
 
 
+# save data for later use
+np.save("saved_data/data-" + timestamp, normalized_image_array) 
+# np.load can turn this back into an array for later use
