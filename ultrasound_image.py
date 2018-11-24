eedf0f12b4c97d46fa9587128a50a09092262ba3
Mackenzie Wilson <mackenziewilson@Mackenzies-MacBook-Air.local>
2018-11-24 18:59:14 -0500

    Got python code ready for Arduino

--- ultrasound_image.py
+++ ultrasound_image.py
@@ -20,55 +20,58 @@ ser = serial.Serial(port)
 
 # Image size
 # MAKE SURE THESE VALUES MATCH UP WITH ARDUINO CODE
-horiz_steps = 6
-vert_steps = 4
+horiz_steps = 10
+vert_steps = 6
 
 # Arrays to hold data (will be spatially upside down)
 data_top = np.zeros((vert_steps, horiz_steps))
 data_bottom = np.zeros((vert_steps, horiz_steps))
 image_array = np.zeros( (vert_steps*2, horiz_steps) )
 
-# Do not start until Arduino tells us to
-x = True
-while x:
-    if np.uint8( ser.readline().strip() ) < 0:
-        x = False
-    
-# START.
-# Read from serial port until enough data captured
-for i in range(vert_steps):
-    for j in range(horiz_steps):
-        data_bottom[i,j] = np.uint8( ser.readline().strip() )
-        data_top[i,j] = np.uint8( ser.readline().strip() )
+while True:
+        
+    # Do not start until Arduino tells us to
+    if np.uint( ser.readline().strip() ) <0:
+        
+        # START.
+        # Read from serial port until enough data captured
+        for i in range(vert_steps):
+            for j in range(horiz_steps):
+                data_bottom[i,j] = np.uint8( ser.readline().strip() )
+                data_top[i,j] = np.uint8( ser.readline().strip() )
+        
+        
+        # Combine two transducer data arrays into one image_array
+        # Keep in mind the data arrays are upside down from how we want the image
+        for i in range(int(vert_steps/2)):
+            image_array[i*4] = data_bottom[i*2]
+            image_array[i*4 + 1] = data_bottom[i*2 + 1]
+            image_array[i*4 + 2] = data_top[i*2]
+            image_array[i*4 + 3] = data_top[i*2 + 1]
+        
+        image_array_final = image_array[::-1]
+        
+        
+        # Normalize image array to pixel values
+        pixel_min = 0
+        pixel_max = 255
+        data_min = np.min(image_array_final)
+        new_data_range = max(np.max(image_array_final) - data_min, 1)
+        normalized_image_array = ( (image_array_final - data_min) * (255/new_data_range) ).astype(np.uint8)
+        
+        
+        # Make image
+        img = im.fromarray(normalized_image_array)
+        img.show()
+        
+        
+        
+        # s=Save results for later use
+        timestamp = time.strftime("%Y%m%d-%H%M%S")
+        img.save("saved_images/image-" + timestamp + ".bmp")
+        np.save("saved_data/data-" + timestamp, normalized_image_array) 
+        # np.load can turn this back into an array for later use
+        
 
-ser.close() # CLOSE THE SERIAL PORT YA DUMMY 
-
-# Combine two transducer data arrays into one image_array
-# Keep in mind the data arrays are upside down from how we want the image
-for i in range(int(vert_steps/2)):
-    image_array[i*4] = data_bottom[i*2]
-    image_array[i*4 + 1] = data_bottom[i*2 + 1]
-    image_array[i*4 + 2] = data_top[i*2]
-    image_array[i*4 + 3] = data_top[i*2 + 1]
-
-image_array_final = image_array[::-1]
 
 
-# Normalize image array to pixel values
-pixel_min = 0
-pixel_max = 255
-data_min = np.min(image_array_final)
-new_data_range = max(np.max(image_array_final) - data_min, 1)
-normalized_image_array = ( (image_array_final - data_min) * (255/new_data_range) ).astype(np.uint8)
-
-# Make image
-img = im.fromarray(normalized_image_array)
-img.show()
-
-
-
-# s=Save results for later use
-timestamp = time.strftime("%Y%m%d-%H%M%S")
-img.save("saved_images/image-" + timestamp + ".bmp")
-np.save("saved_data/data-" + timestamp, normalized_image_array) 
-# np.load can turn this back into an array for later use
