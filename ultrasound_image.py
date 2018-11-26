b1862f6d13c090bbcd718653b47980e83e503b05
Mackenzie Wilson <mackenziewilson@Mackenzies-MacBook-Air.local>
2018-11-25 19:43:32 -0500

    Refactored python code to work offline

--- ultrasound_image.py
+++ ultrasound_image.py
@@ -12,68 +12,151 @@ import serial
 import numpy as np
 import PIL.Image as im
 import time 
-
-
-# Set up serial to read from Arduino
-port = '/dev/cu.usbmodem1421'
-ser = serial.Serial(port)
-
-# Image size
-# MAKE SURE THESE VALUES MATCH UP WITH ARDUINO CODE
-horiz_steps = 10
-vert_steps = 7
-
-# Arrays to hold data (will be spatially upside down)
-data_top = np.zeros((vert_steps, horiz_steps))
-data_bottom = np.zeros((vert_steps, horiz_steps))
-image_array = np.zeros( (vert_steps*2, horiz_steps) )
-
-while True:
-        
-    # Do not start until Arduino tells us to
-    if np.uint8( ser.readline().strip() ) == np.uint8(99999):
-        
-        # START.
-        # Read from serial port until enough data captured
-        for i in range(vert_steps):
-            for j in range(horiz_steps):
-                data_bottom[i,j] = np.uint8( ser.readline().strip() )
-                print(data_bottom[i,j])
-                data_top[i,j] = np.uint8( ser.readline().strip() )
-                print(data_top[i,j])
-        
-        
-        # Combine two transducer data arrays into one image_array
-        # Keep in mind the data arrays are upside down from how we want the image
-        for i in range(int(vert_steps/2)):
-            image_array[i*4] = data_bottom[i*2]
-            image_array[i*4 + 1] = data_bottom[i*2 + 1]
-            image_array[i*4 + 2] = data_top[i*2]
-            image_array[i*4 + 3] = data_top[i*2 + 1]
-        
-        image_array_final = image_array[::-1]
-        
-        
-        # Normalize image array to pixel values
-        pixel_min = 0
-        pixel_max = 255
-        data_min = np.min(image_array_final)
-        new_data_range = max(np.max(image_array_final) - data_min, 1)
-        normalized_image_array = ( (image_array_final - data_min) * (255/new_data_range) ).astype(np.uint8)
-        
-        
-        # Make image
-        img = im.fromarray(normalized_image_array)
-        img.show()
-        
-        
-        
-        # s=Save results for later use
-        timestamp = time.strftime("%Y%m%d-%H%M%S")
-        img.save("saved_images/image-" + timestamp + ".bmp")
-        np.save("saved_data/data-" + timestamp, normalized_image_array) 
-        # np.load can turn this back into an array for later use
-        
+import random
 
 
 
+def normalize_array(arr):
+     pixel_max = 255
+     data_min = np.min(arr)
+     new_data_range = max(np.max(arr) - data_min, 1)
+     new_arr = (arr - data_min) * (pixel_max/new_data_range)
+     return (pixel_max-new_arr).astype(np.uint8)
+
+
+def bin_pixels(arr):
+    assert(type(arr) == np.ndarray)
+    min_dist = 100 # in mm
+    #max_dist = 300
+    pixel_min = 0
+    pixel_max = 255
+    
+    result_arr = arr.astype(int) - min_dist
+    result_arr[result_arr < pixel_min] = pixel_min
+    result_arr[result_arr > pixel_max] = pixel_max
+    return (pixel_max - result_arr).astype(np.uint8)
+
+
+def build_image_array(bottom, top, h, v):
+    # Combine two transducer data arrays into one image_array
+    # Keep in mind the data arrays are upside down from how we want the image
+    im_arr = np.zeros( (v*2, h) )
+    for i in range(int(v/2)):
+        im_arr[i*4] = bottom[i*2]
+        x = bottom[i*2 + 1]
+        im_arr[i*4 + 1] = x[::-1] # This row went backwards
+        im_arr[i*4 + 2] = top[i*2]
+        y = top[i*2 + 1]
+        im_arr[i*4 + 3] = y[::-1] # This row went backwards
+        
+    return im_arr[::-1]
+
+
+def get_image(arr):
+    # This also saves the data and image
+    img = im.fromarray(arr)
+    img.show()
+    
+    # Save results for later use
+    timestamp = time.strftime("%Y%m%d-%H%M%S")
+    img.save("saved_images/image-" + timestamp + ".bmp")
+    np.save("saved_data/data-" + timestamp, arr) 
+    # np.load can turn this back into an array for later use
+    
+    
+def load_image_data(path, horiz_steps=10):
+    # Normalized numpy files
+    arr = (np.load(path)).astype(np.uint8)
+    return arr.reshape(int(arr.size / horiz_steps), horiz_steps )
+
+
+def load_raw_data(path, h=10, v=8):
+    arr = np.genfromtxt(path, delimiter=",")
+    arr = arr[1:]
+    bot = arr[0::2]
+    top = arr[1::2]
+    
+    return build_image_array(bot.reshape(v,h), top.reshape(v,h), h, v)
+
+
+def load_raw_data_horizontal_testing(path, horiz_steps=10):
+    arr = np.genfromtxt (path, delimiter=",")
+    arr = arr[1:]
+    
+    # The data has 5 values for each transducer at each location, bottom first 
+    # then top. The data contains two passes, right then left.
+    right = arr[0:20]
+    left = arr[20:40]
+    left = left[::-1]
+    
+    im_arr = np.zeros((4, horiz_steps))
+    
+    for i in range(len(im_arr[0])):
+        im_arr[0,i] = right[i*2] #bottom transducer
+        im_arr[1,i] = right[i*2 + 1] #top transducer
+        im_arr[2,i] = left[i*2]
+        im_arr[3,i] = left[i*2 + 1]
+
+    return im_arr.astype(np.uint8)
+
+
+def gen_random_im_array(x=10, y=8):
+    high_lim = 500
+    low_lim = 5
+    arr = np.zeros((y*2, x))
+    for col in range(x):
+        for row in range(y*2):
+            arr[row, col] = random.randint(low_lim, high_lim)
+
+    return arr.astype(np.uint8)
+
+
+def display_random_binned_and_norm():
+    x = gen_random_im_array()
+    x_norm = normalize_array(x)
+    x_binned = bin_pixels(x)
+    get_image(x_norm)
+    get_image(x_binned)
+    
+
+
+        
+if __name__ == '__main__':
+    # Set up serial to read from Arduino
+    port = '/dev/cu.usbmodem1421'
+    ser = serial.Serial(port)
+    
+    # Image size
+    # MAKE SURE THESE VALUES MATCH UP WITH ARDUINO CODE
+    horiz_steps = 10
+    vert_steps = 8
+    
+    # Arrays to hold data (will be spatially upside down)
+    data_top = np.zeros((vert_steps, horiz_steps))
+    data_bottom = np.zeros((vert_steps, horiz_steps))
+
+
+    while True:
+        
+        # Do not start until Arduino tells us to
+        if np.uint8( ser.readline().strip() ) == np.uint8(99999):
+            
+            # START.
+            # Read from serial port until enough data captured
+            for i in range(vert_steps):
+                for j in range(horiz_steps):
+                    data_bottom[i,j] = np.uint8( ser.readline().strip() )
+                    print(data_bottom[i,j])
+                    data_top[i,j] = np.uint8( ser.readline().strip() )
+                    print(data_top[i,j])
+            
+            # Combine data into image array
+            image_array = build_image_array(data_bottom, data_top, horiz_steps, vert_steps)
+            np.save("saved_data/raw-data1", image_array) 
+            
+            # Normalize image array to pixel values
+            normalized_image_array = bin_pixels(image_array)
+    
+            # Make image
+            get_image(normalized_image_array)
+            
